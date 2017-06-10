4141e30d7f93671478aa18cb5badb2f5bee51957
195024 <195024@edu.p.lodz.pl>
2017-06-10 19:38:23 +0200

    Initial commit, test0 itCompiles

---
+++ test/bowlingGame/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package bowlingGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+	
+	@Test
+	public void itCompiles() {
+		assertTrue(true);
+	}
+}
